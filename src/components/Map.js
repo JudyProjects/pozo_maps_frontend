2a720a707cfa40196ebc39a650f52f6cf0d00281
Franco Sancristobal <franco.sancris29@gmail.com>
2024-10-26 16:51:59 -0300

    [mapaLeaflet] pasaje laptop

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
+import "leaflet-routing-machine";
 import constants from './Map_Components/constants';
+import "leaflet/dist/leaflet.css";
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 
 const GeocoderControl = () => {
     const map = useMap(); // Obtener el mapa usando el hook de react-leaflet
+    const initialized = useRef(false);
 
     useEffect(() => {
-        if (!map) return;
+        if (!initialized.current) {
+            initialized.current = true;
+            // Configurar el icono predeterminado para los marcadores
+            L.Marker.prototype.options.icon = constants;
 
-        // Configurar el icono predeterminado para los marcadores
-        L.Marker.prototype.options.icon = constants;
+            // Añadir el geocoder al mapa
+            const routingControl = L.Routing.control({
+                routeWhileDragging: true,
+                showAlternatives: false,
+                router: new L.Routing.mapbox('pk.eyJ1IjoiZnJhbmNvLXNhbmNyaXMyOSIsImEiOiJjbTJhendnc3cwbXFsMmtxNHBzYm80ZGdtIn0.gHW9ZszrPfh0cUs-zVvI3Q'),
+                geocoder: L.Control.Geocoder.nominatim(),
+                lineOptions: {
+                    styles: [{ color: 'green', weight: 3 }] // Define el estilo de la línea
+                }
+            }).addTo(map);
 
-        // Añadir el geocoder al mapa
-        L.Control.geocoder({
-            defaultMarkGeocode: false
-        })
-        .on('markgeocode', function (e) {
-            const latlng = e.geocode.center;
-            L.marker(latlng).addTo(map).bindPopup(e.geocode.name).openPopup();
-            map.fitBounds(e.geocode.bbox); // Ajustar el mapa a los límites del geocódigo
-        })
-        .addTo(map);
-    }, [map]);
+            
+        }
+    });
 
-    return null; // No necesitamos renderizar nada explícitamente
+    
+    return null; // No necesita renderizar nada explícitamente
 };
 
 const Map = () => {
@@ -35,11 +43,12 @@ const Map = () => {
     return (
         <MapContainer
             center={position}
-            zoom={6}
+            zoom={2}
+            minZoom={7}
             style={{ height: '700px' }}
             id='mapId'
         >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <TileLayer url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png" attribution='@OpenStreetMap, @StadiaMaps' />
             <GeocoderControl /> {/* Añadir el componente de control del geocoder */}
         </MapContainer>
     );
